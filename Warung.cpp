12ffa9d3231cd9979c4e412afcca03dbb0fe9d96
FajarRAP <101798306+FajarRAP@users.noreply.github.com>
2022-03-24 16:11:14 +0700

    update kalkulasi menu

--- Warung.cpp
+++ Warung.cpp
@@ -2,12 +2,37 @@
 using namespace std;
  
  int main(){
- 	int harga,ayambakar = 21000,ayamgoreng = 17000,pilih;
- 	menu:
- 		cout<<"Daftar menu : "<<endl;
- 		cout<<"1.Ayam Goreng "<<endl;
- 		cout<<"2.Ayam Bakar"<<endl;
- 		cout<<"Silahkan Pilih : ";
- 		cin>>pilih;
- 	
- }
+  int harga, ayambakar = 21000, ayamgoreng = 17000, pilih, pcs, harga, total, diskon;
+  menu: 
+  cout<<"Daftar menu : "<<endl;
+		cout<<"1. Ayam Goreng "<<endl;
+ 	cout<<"2. Ayam Bakar"<<endl;
+  cout<<"3. Bayar"<<endl;
+ 	cout<<"Silahkan Pilih : ";
+ 	cin>>pilih;
+  switch(pilih){
+   case 1:
+    cout<<"Pcs : ";
+    cin>>pcs;
+    harga=pcs*ayamgoreng;
+    total+=harga;
+    goto menu;
+    break;
+   case 2:
+    cout<<"Pcs : ";
+    cin>>pcs;
+    harga=pcs*ayambakar;
+    total+=harga;
+    break;
+   case 3:
+    break;
+    if(total>45000){
+      diskon=total*0.1;
+      total-=diskon;
+      
+    }
+   default:
+    cout<<"Menu tidak tersedia"<<endl;
+  }
+ 	return 0;
+}
