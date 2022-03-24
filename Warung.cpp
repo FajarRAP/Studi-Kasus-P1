215143a83215b94c711da81393452f3bfda900a8
GagahAryoWijoseno-152 <101798471+GagahAryoWijoseno-152@users.noreply.github.com>
2022-03-24 16:00:43 +0700

    Menambah Menu

--- Warung.cpp
+++ Warung.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+ 
+ int main(){
+ 	int harga,ayambakar = 21000,ayamgoreng = 17000,pilih;
+ 	menu:
+ 		cout<<"Daftar menu : "<<endl;
+ 		cout<<"1.Ayam Goreng "<<endl;
+ 		cout<<"2.Ayam Bakar"<<endl;
+ 		cout<<"Silahkan Pilih : ";
+ 		cin>>pilih;
+ 	
+ }
