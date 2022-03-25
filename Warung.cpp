267a94c0a89de3e9dee4b7745af09dd4366ce143
Fajar Riansyah <101798306+FajarRAP@users.noreply.github.com>
2022-03-25 10:28:34 +0000

    merapihkan baris

--- Warung.cpp
+++ Warung.cpp
@@ -1,54 +1,54 @@
 #include <iostream>
 using namespace std;
 
- void garis(int a){
- 	for(int b=0; b<a; b++){
-		cout<<"=";
-	}
-	cout<<endl;
- }
+void garis(int a){
+  for(int b=0; b<a; b++){
+  cout<<"=";
+  }
+  cout<<endl;
+}
 
- int main(){
- int ayambakar = 21000, ayamgoreng = 17000, pilih, pcs, harga, total, diskon;
- menu: 
- cout<<"Daftar menu : "<<endl;
- cout<<"1. Ayam Goreng "<<endl;
- cout<<"2. Ayam Bakar"<<endl;
- cout<<"3. Bayar"<<endl;
- cout<<"Silahkan Pilih : ";
- cin>>pilih;
- switch(pilih){
- 	case 1:
-		cout<<"Pcs : ";
+int main(){
+int ayambakar = 21000, ayamgoreng = 17000, pilih, pcs, harga, total, diskon;
+menu: 
+cout<<"Daftar menu : "<<endl;
+cout<<"1. Ayam Goreng "<<endl;
+cout<<"2. Ayam Bakar"<<endl;
+cout<<"3. Bayar"<<endl;
+cout<<"Silahkan Pilih : ";
+cin>>pilih;
+switch(pilih){
+  case 1:
+    cout<<"Pcs : ";
 		cin>>pcs;
 		harga=pcs*ayamgoreng;
 		total+=harga;
 		goto menu;
 		break;
-  	case 2:
+  case 2:
 		cout<<"Pcs : ";
 		cin>>pcs;
 		harga=pcs*ayambakar;
 		total+=harga;
 		goto menu;
 		break;
-   	case 3:
-		 if(total>45000){
-      			diskon=total*0.1;
-      			total-=diskon;
+  case 3:
+    if(total>45000){
+      diskon=total*0.1;
+      total-=diskon;
 			garis(30);
 			cout<<"    PEMBAYARAN    "<<endl;
 			garis(30);
 			cout<<"TOTAL : "<<total<<endl;
-    		}else{
+    }else{
 			garis(30);
 			cout<<"    PEMBAYARAN    "<<endl;
 			garis(30);
 			cout<<"TOTAL : "<<total<<endl;
-		}
-    		break;
+	  }
+    break;
    	default:
-    		cout<<"Menu tidak tersedia"<<endl;
+      cout<<"Menu tidak tersedia"<<endl;
   }
  	return 0;
 }
