65397cbdcd5b4e08aa33f24843953df266d8d818
FajarRAP <101798306+FajarRAP@users.noreply.github.com>
2022-03-25 18:13:48 +0700

    nambah system cls, pcs, dan rapihin struk

--- Warung.cpp
+++ Warung.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 void garis(int a){
-  for(int b=0; b<a; b++){
-  cout<<"=";
-  }
-  cout<<endl;
+	for(int b=0; b<a; b++){
+  	cout<<"=";
+  	}
+  	cout<<endl;
 }
 
 int main(){
 int ayambakar = 21000, ayamgoreng = 17000, pilih, pcs, harga, total, diskon;
+int pcsag=0, pcsab=0;
 menu: 
+system("cls");
 cout<<"Daftar menu : "<<endl;
 cout<<"1. Ayam Goreng "<<endl;
 cout<<"2. Ayam Bakar"<<endl;
@@ -18,37 +20,38 @@ cout<<"3. Bayar"<<endl;
 cout<<"Silahkan Pilih : ";
 cin>>pilih;
 switch(pilih){
-  case 1:
-    cout<<"Pcs : ";
+	case 1:
+		cout<<"Pcs : ";
 		cin>>pcs;
+		pcsag+=pcs;
 		harga=pcs*ayamgoreng;
 		total+=harga;
 		goto menu;
 		break;
-  case 2:
+  	case 2:
 		cout<<"Pcs : ";
 		cin>>pcs;
+		pcsab+=pcs;
 		harga=pcs*ayambakar;
 		total+=harga;
 		goto menu;
 		break;
-  case 3:
-    if(total>45000){
-      diskon=total*0.1;
-      total-=diskon;
-			garis(30);
-			cout<<"    PEMBAYARAN    "<<endl;
-			garis(30);
+  	case 3:	
+		garis(30);
+		cout<<"\t\tPembayaran"<<endl;
+		garis(30);
+		cout<<"Ayam Goreng x"<<pcsag<<endl;
+		cout<<"Ayam Bakar  x"<<pcsab<<endl;
+		if(total>45000){
+			diskon=total*0.1;
+			total-=diskon;
 			cout<<"TOTAL : "<<total<<endl;
-    }else{
-			garis(30);
-			cout<<"    PEMBAYARAN    "<<endl;
-			garis(30);
+		}else{
 			cout<<"TOTAL : "<<total<<endl;
-	  }
-    break;
-   	default:
-      cout<<"Menu tidak tersedia"<<endl;
+		}
+		break;
+		default:
+			cout<<"Menu tidak tersedia"<<endl;
   }
  	return 0;
 }
